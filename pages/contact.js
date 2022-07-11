434a7d41e3dfa1adfec1d7762dc097f6900f1433
mohansky <38560439+mohansky@users.noreply.github.com>
2022-07-11 11:38:00 +0530

    Add files via upload

---
+++ pages/contact.js
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import Address from "../components/address";
+import Timings from "../components/timings";
+import Form from "../components/form";
+
+export default function Contact() {
+  return (
+    <>
+      <Head>
+        <title>La Volotte | Kontakt</title>
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+      </Head>
+      <section className="section pt-0">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-3 col-sm-6">
+              <Address />
+            </div>
+            <div className="col-lg-5 col-sm-6">
+              <Timings />
+            </div>
+            <div className="col-lg-4 ">
+              <h3 className="mb-4">Kontakt</h3>
+              <Form />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
