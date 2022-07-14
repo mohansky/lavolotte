df526a4ade08ca42121f45a8fa8071b7dc36e0e8
mohansky <38560439+mohansky@users.noreply.github.com>
2022-07-14 14:12:22 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
     env: {
         API_TOKEN: process.env.SNIPCART_KEY,
     },
+    reactStrictMode: true,
+    images: {
+      formats: ['image/avif', 'image/webp'],
+    }
   }
