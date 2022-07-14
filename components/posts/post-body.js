eefbcb476c3da1f90008788455997e510c4c4564
mohansky <38560439+mohansky@users.noreply.github.com>
2022-07-14 13:03:01 +0530

    Delete components directory

--- components/posts/post-body.js
+++
@@ -1,7 +0,0 @@
-export default function PostBody({ content }) {
-  return (
-    <div className="mx-auto py-5">
-      <div dangerouslySetInnerHTML={{ __html: content }}/>
-    </div>
-  )
-}
--- components/products/product-title.js
+++
@@ -1,7 +0,0 @@
-export default function ProductTitle({ children }) {
-  return (
-    <h1 className=" ">
-      {children}
-    </h1>
-  )
-}
