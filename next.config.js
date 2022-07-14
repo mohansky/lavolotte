78d7c009324008f95129d43d38bf7f7d8bc4d832
mohansky <38560439+mohansky@users.noreply.github.com>
2022-07-14 17:57:55 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,9 +1,26 @@
-module.exports = {
-    env: {
-        API_TOKEN: process.env.SNIPCART_KEY,
-    },
-    reactStrictMode: true,
-    images: {
-      formats: ['image/avif', 'image/webp'],
-    }
-  }
+// module.exports = {
+//     env: {
+//         API_TOKEN: process.env.SNIPCART_KEY,
+//     },
+//     reactStrictMode: true,
+//     images: {
+//       formats: ['image/avif', 'image/webp'],
+//     }
+//   }
+
+
+  // next.config.js
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
+  env: {
+    API_TOKEN: process.env.SNIPCART_KEY,
+},
+reactStrictMode: true,
+images: {
+  formats: ['image/avif', 'image/webp'],
+}
+});
+
